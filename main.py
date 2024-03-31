a2b86d73eb4222d66e30a571a2f89d4f2e118b0d
Faunly <faunlyanderson@gmail.com>
2024-03-31 11:12:47 +0300

    Создание программы работающая с API сайта openlibrary.org

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-# This is a sample Python script.
+import requests
+# Пользовательский запрос
+user_query = input("Введите название книги: ")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Параметры запроса
+params = {'q': f'"{user_query}"',
+          'mode': 'everything'}
+
+# Header -> User-Agent взят с httpbin.org
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)\
+    Chrome/122.0.0.0 Safari/537.36"
+  }
+
+# Запрос с параметрами и заголовком(.json - говорит сайту использовать application/json, а не text/html)
+responce = requests.get('https://openlibrary.org/search.json', params=params, headers=headers)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-f
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Проверка на правильность запроса
+if responce.status_code == 200:
+    # print(responce.json())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    query = responce.json()['q']
+    num_found = responce.json()['num_found']
+
+    print(f"По запросу: {query}")
+    print(f"Найдено: {num_found} результат(ов)")
+
+    for i in range(num_found):
+        print(responce.json()['docs'][i])  # Title
